b8ef36ef10dd11c57b5b4b651e229b1392381ac9
Cristian Marsico <cristianmarsico84@gmail.com>
2021-09-12 17:52:52 -0300

    lapiz
    se creó la funcionalidad de dibujar

--- js/index.js.js
+++ js/index.js.js
@@ -1,75 +1,34 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function (e) {
-
   //-------------------------------CANVAS PRINCIPAL (LADO IZQ)------------------------------------
   let canvasPricipal = document.querySelector("#canvasPrincipal");
-  let ctx = canvasPricipal.getContext("2d");//contexto para la superficie de dibujo
+  let ctx = canvasPricipal.getContext("2d"); //contexto para la superficie de dibujo
   let width = canvasPricipal.width;
   let height = canvasPricipal.height;
 
   //dejo el canvas en blanco
   let matriz = ctx.getImageData(0, 0, width, height); //capturo la matriz
-  let imagenPrincipal = limpiar(matriz); 
+  let imagenPrincipal = limpiar(matriz);
   ctx.putImageData(imagenPrincipal, 0, 0);
   //--------------------------------------------------------------------------------------------//
 
-   //-------------------------------CANVAS EDITABLE (LADO DER)-------------------------------------
- let canvasEditable = document.querySelector("#canvasEditable");
- let ctxEditable = canvasEditable.getContext("2d"); //contexto para la superficie de dibujo
- let widthEditable = canvasEditable.width;
- let heightEditable = canvasEditable.height;
+  //-------------------------------CANVAS EDITABLE (LADO DER)-------------------------------------
+  let canvasEditable = document.querySelector("#canvasEditable");
+  let ctxEditable = canvasEditable.getContext("2d"); //contexto para la superficie de dibujo
+  let widthEditable = canvasEditable.width;
+  let heightEditable = canvasEditable.height;
 
- //dejo el canvas en blanco
- let matrizEditable = ctxEditable.getImageData(0, 0, widthEditable, heightEditable);
- let imagenSecundaria = limpiar(matrizEditable);
- ctxEditable.putImageData(imagenSecundaria, 0, 0);
- //--------------------------------------------------------------------------------------------//
-
- //------------------------------------------BOTONES---------------------------------------------
- /*
-  * BOTON GRIS
-  */
- document.querySelector("#btnGris").addEventListener("click", (e) => {
-  let imagenEditada = obtenerGrises();
-  ctxEditable.putImageData(imagenEditada, 0, 0);
-});
-//--------------------------------------------------------------------------------------------//
-
-
-//------------------------------------FUNCION COLORES-------------------------------------------
-  /*
-   *FUNCION PARA COLOR GRIS 
-   */
-   function obtenerGrises(){
-    let matriz = ctx.getImageData(0, 0, width, height);
-    for(let i = 0; i < width; i++){
-      for(let j = 0; j < height; j++){
-        let pixel = obtenerPixel(matriz, i, j);
-        let valor = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3);
-        let tamanio = verificarTamanio(valor);
-        let r = tamanio;
-        let g = tamanio;
-        let b = tamanio;
-        let a = 255;
-        editarPixel(matriz, i, j , r, g, b , a);
-      }
-    }
-    return matriz;
-  }
-
-  //-------------------------------------VERIFICAR TAMAÑO---------------------------------------
-  function verificarTamanio(tamanio){
-    if(tamanio > 255){
-      tamanio = 255;  
-    }
-
-    if(tamanio < 0){
-        tamanio = 0;
-    }
-    return tamanio;
-  }
-  //-------------------------------------------------------------------------------------------//
+  //dejo el canvas en blanco
+  let matrizEditable = ctxEditable.getImageData(
+    0,
+    0,
+    widthEditable,
+    heightEditable
+  );
+  let imagenSecundaria = limpiar(matrizEditable);
+  ctxEditable.putImageData(imagenSecundaria, 0, 0);
+  //--------------------------------------------------------------------------------------------//
 
   //------------------------------------> FUNCION LIMPIAR <-------------------------------------
   function limpiar(imagen) {
@@ -88,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
   //-------------------------------------EDITAR PIXELES DEL CANVAS-----------------------------
   function editarPixel(imageData, x, y, r, g, b, a) {
-    let index = (x + y * imageData.height) * 4;//convierto la matriz en un array
+    let index = (x + y * imageData.height) * 4; //convierto la matriz en un array
     imageData.data[index] = r;
     imageData.data[index + 1] = g;
     imageData.data[index + 2] = b;
@@ -96,19 +55,40 @@ document.addEventListener("DOMContentLoaded", function (e) {
   }
   //-------------------------------------------------------------------------------------------//
 
-   //-----------------------------------OBTENER PIXELES DEL CANVAS-------------------------------
-   function obtenerPixel(imageData, x, y) {
+  //-----------------------------------OBTENER PIXELES DEL CANVAS-------------------------------
+  function obtenerPixel(imageData, x, y) {
     let index = (x + y * imageData.height) * 4; //convierto la matriz en un array
     let r = imageData.data[index + 0];
     let g = imageData.data[index + 1];
     let b = imageData.data[index + 2];
     let a = imageData.data[index + 3];
 
-    return [r,g,b,a];
+    return [r, g, b, a];
   }
   //-------------------------------------------------------------------------------------------//
-  
-  //---------------------------------CARGAR IMAGEN DESDE ORDENADOR-----------------------------
+
+  //------------------------------------------BOTONES---------------------------------------------
+  /*
+   * BOTON GRIS
+   */
+  document.querySelector("#btnGris").addEventListener("click", (e) => {
+    let imagenEditada = obtenerGrises();
+    ctxEditable.putImageData(imagenEditada, 0, 0);
+  });
+
+  /*
+   *BOTON LAPIZ
+   */
+  let lapiz = document.querySelector("#lapiz");
+  let click = false;
+  let dibujando = false;
+  let borrado = false;
+  //--------------------------------------------------------------------------------------------//
+
+  //---------------------------------------EVENTOS--------------------------------------------
+  /*
+   *CARGAR IMAGEN DESDE ORDENADOR
+   */
   document.querySelector("#inputFile").addEventListener("change", (e) => {
     const ARCHIVO = document.querySelector("#inputFile").files[0];
     const OBJETO = new FileReader();
@@ -121,18 +101,106 @@ document.addEventListener("DOMContentLoaded", function (e) {
         let imagen = new Image();
         imagen.src = OBJETO.result;
         imagen.onload = function () {
-            myDrawImageMethod(imagen);
+          myDrawImageMethod(imagen);
         };
       },
       false
     );
   });
-  //-------------------------------------------------------------------------------------------
+
+  /**
+   * LAPIZ
+   */
+  lapiz.addEventListener("click", (e) => {
+    dibujando = true;
+    borrado = false;
+    dibujarLinea();
+  });
+
+  //-------------------------------------------------------------------------------------------//
 
   //--------------------------------------DIBUJAR----------------------------------------------
   function myDrawImageMethod(imagen) {
     ctx.drawImage(imagen, 0, 0, width, height);
   }
-  //-------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------//
 
+  //---------------------------------------EVENTOS MOUSE----------------------------------------
+  function dibujarLinea() {
+    if (!click) {
+      canvasEditable.addEventListener("mousedown", (e) => {
+        ctxEditable.beginPath(); //INICIA EL CAMINO O DIBUJO MIENTRAS EL CLICK ESTE PRECIONADO
+        click = true;
+      });
+    }
+
+    canvasEditable.addEventListener("mousemove", (e) => {
+      if (click) {
+        dibujar(e);
+      }
+    });
+
+    canvasEditable.addEventListener("mouseup", (e) => {
+      click = false;
+      ctxEditable.closePath(); //FINALIZO EL CAMINO O DIBUJO CUANDO SUELTO EL CLICK
+    });
+
+    function dibujar(e) {
+      let x = e.layerX;
+      let y = e.layerY;
+      trazarLinea(x, y);
+    }
+
+    function trazarLinea(x, y) {
+      if (dibujando) {
+        let color = document.getElementById("colores").value;
+        let anchoLapiz = document.getElementById("rango").value;
+        ctxEditable.strokeStyle = color;
+        ctxEditable.lineWidth = anchoLapiz;
+      }
+      if (click) {
+        ctxEditable.lineTo(x, y);
+        ctxEditable.stroke();
+      }
+    }
+  }
+  //-------------------------------------------------------------------------------------------//
+
+
+
+  //------------------------------------FUNCION COLORES-------------------------------------------
+  /*
+   *FUNCION PARA COLOR GRIS
+   */
+  function obtenerGrises() {
+    let matriz = ctx.getImageData(0, 0, width, height);
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        let pixel = obtenerPixel(matriz, i, j);
+        let valor = Math.floor((pixel[0] + pixel[1] + pixel[2]) / 3);
+        let tamanio = verificarTamanio(valor);
+        let r = tamanio;
+        let g = tamanio;
+        let b = tamanio;
+        let a = 255;
+        editarPixel(matriz, i, j, r, g, b, a);
+      }
+    }
+    return matriz;
+  }
+  //-------------------------------------------------------------------------------------------//
+  
+
+  //-------------------------------------VERIFICAR TAMAÑO---------------------------------------
+  function verificarTamanio(tamanio) {
+    if (tamanio > 255) {
+      tamanio = 255;
+    }
+
+    if (tamanio < 0) {
+      tamanio = 0;
+    }
+    return tamanio;
+  }
+  //-------------------------------------------------------------------------------------------//
 });
