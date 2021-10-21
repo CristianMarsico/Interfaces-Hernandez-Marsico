a073cb56ad45dbcd966e50f0427fe2499f7c3732
Cristian Marsico <cristianmarsico84@gmail.com>
2021-10-21 18:47:36 -0300

    se crea la clase Personaje, Avatar y el main

---
+++ Entregable/TPE_3/js/Avatar.js
@@ -0,0 +1,67 @@
+class Avatar extends Personaje {
+  constructor(domId) {
+    super(domId);
+  }
+  
+  init() {
+    this.div.classList.remove('die');
+    this.div.classList.add('run');
+    this.div.style.display = 'block';
+    this.div.style.left = '100px';
+    this.div.style.bottom = '5px';
+  }
+
+  /* Obtengo la posicion del div del avatar y la comparo con la posicion del obtaculo */
+  checkCollision(element) {
+    this.updatePosition();
+    element.updatePosition();
+    
+
+        console.log("yo "+ this.position.left);
+        console.log("araña "+ element.position.left);
+
+
+
+    if (this.position.left  < element.position.left + element.width  &&
+      this.position.left + this.width > element.position.left &&
+      this.position.top < element.position.top + element.height &&
+      this.position.top + this.height > element.position.top) {
+      return true;
+    }
+    return false;
+  }
+  move(key) {
+    this.updatePosition();
+    switch (key) {
+      case 37:
+        this.div.style.left = this.position.left - 20 + 'px';
+        break;
+        
+      case 39:
+        this.div.style.left = this.position.left + 20 + 'px';
+        break;
+        
+      default:
+        break;
+    }
+  }
+  jump() {
+    this.div.classList.add('jump');
+    setTimeout(() => {
+      this.div.classList.remove('jump');
+    }, 700);
+  }
+  die() {
+    /*AUDIO*/
+    //this.dieAudio.play();
+    this.div.classList.add('die');
+    setTimeout(() => {
+      this.div.classList.remove('die');
+    }, 1200);
+  }
+  stop() {
+    this.div.style.left = '100px';
+    this.div.style.bottom = '100px';
+    this.div.classList.remove('run');
+  }
+}
---
+++ Entregable/TPE_3/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  let loop = false;
+  const avatar = new Avatar('avatar'); //Instancio el avatar
+ 
+
+
+  function start() {
+    avatar.init();
+    
+    loop = true;
+    
+   
+  }
+
+  
+    start();
+  
+  
+  
+  
+});
