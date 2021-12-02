adc07dcd896aaf3416e292d333f899d4d6f83206
Francisco Hernandez <mihaelkyeah@gmail.com>
2021-12-02 02:43:43 -0300

    Agregado publicación con galería, agregado búsqueda avanzada

--- Entregable/TPE_5/js/main.js
+++ Entregable/TPE_5/js/main.js
@@ -8,27 +8,37 @@ let btn_menu_more_1 = document.querySelector('#boton-menu-more-1');
 let menu_more_1 = document.querySelector('#menu-more-1');
 let btn_menu_more_2 = document.querySelector('#boton-menu-more-2');
 let menu_more_2 = document.querySelector('#menu-more-2');
+let btn_menu_more_3 = document.querySelector('#boton-menu-more-3');
+let menu_more_3 = document.querySelector('#menu-more-3');
 
 let puntaje_init_post_1 = 120;
 let puntaje_init_post_2 = 63;
+let puntaje_init_post_3 = -1;
 let post_1_puntuacion = document.querySelector('#post_1_puntuacion');
 let post_2_puntuacion = document.querySelector('#post_2_puntuacion');
+let post_3_puntuacion = document.querySelector('#post_3_puntuacion');
 let post_1_megusta_boton = document.querySelector('#post_1_megusta_boton');
 let post_2_megusta_boton = document.querySelector('#post_2_megusta_boton');
+let post_3_megusta_boton = document.querySelector('#post_3_megusta_boton');
 let post_1_nomegusta_boton = document.querySelector('#post_1_nomegusta_boton');
 let post_2_nomegusta_boton = document.querySelector('#post_2_nomegusta_boton');
+let post_3_nomegusta_boton = document.querySelector('#post_3_nomegusta_boton');
 let post_1_megusta_img = document.querySelector('#post_1_megusta_img');
 let post_2_megusta_img = document.querySelector('#post_2_megusta_img');
+let post_3_megusta_img = document.querySelector('#post_3_megusta_img');
 let post_1_nomegusta_img = document.querySelector('#post_1_nomegusta_img');
 let post_2_nomegusta_img = document.querySelector('#post_2_nomegusta_img');
+let post_3_nomegusta_img = document.querySelector('#post_3_nomegusta_img');
 
 let listaMenus = [];
 listaMenus[0] = menu_more_1;
 listaMenus[1] = menu_more_2;
+listaMenus[2] = menu_more_3;
 
 document.addEventListener('DOMContentLoaded', ()=> {
   post_1_puntuacion.innerHTML = puntaje_init_post_1;
   post_2_puntuacion.innerHTML = puntaje_init_post_2;
+  post_3_puntuacion.innerHTML = puntaje_init_post_3;
 })
 
 post_1_megusta_boton.addEventListener('click', (event) => {
@@ -49,6 +59,16 @@ post_2_megusta_boton.addEventListener('click', (event) => {
 post_2_nomegusta_boton.addEventListener('click', (event) => {
   subirBajarPuntaje(post_2_puntuacion, puntaje_init_post_2, false, event);
   resaltarBoton(post_2_nomegusta_img);
+});
+
+post_3_megusta_boton.addEventListener('click', (event) => {
+  subirBajarPuntaje(post_3_puntuacion, puntaje_init_post_3, true, event);
+  resaltarBoton(post_3_megusta_img);
+});
+
+post_3_nomegusta_boton.addEventListener('click', (event) => {
+  subirBajarPuntaje(post_3_puntuacion, puntaje_init_post_3, false, event);
+  resaltarBoton(post_3_nomegusta_img);
 });
 
 function subirBajarPuntaje(puntaje, puntajeinit, subir, event) {
@@ -99,6 +119,9 @@ btn_menu_more_1.addEventListener('click', function(event) {
 btn_menu_more_2.addEventListener('click', function(event) {
   abrirMenuMore(menu_more_2, event);
 });
+btn_menu_more_3.addEventListener('click', function(event) {
+  abrirMenuMore(menu_more_3, event);
+});
 
 function abrirMenuMore(menu_more, event) {
   event.preventDefault();
@@ -127,4 +150,24 @@ function hideOnClickOutside(element) {
   const removeClickListener = () => {
       document.removeEventListener('click', outsideClickListener)
   }
+}
+
+/**
+ * =============== BÚSQUEDA AVANZADA ===============
+ */
+
+
+let input_buscar = document.querySelector('#input_buscar');
+let busqueda_avanzada = document.querySelector('#busqueda_avanzada');
+
+input_buscar.addEventListener("click", (event) => {
+  mostrarOcultarBusquedaAvanzada(event)
+});
+
+function mostrarOcultarBusquedaAvanzada(event) {
+  event.preventDefault();
+  if(busqueda_avanzada.hidden)
+    busqueda_avanzada.removeAttribute('hidden');
+  else  
+    busqueda_avanzada.setAttribute('hidden','true');
 }
